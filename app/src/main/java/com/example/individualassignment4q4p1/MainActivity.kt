fb6b3082811626ae119eb28809e3ca496448c277
Pard3sh <pard3sh@bu.edu>
2026-03-31 12:54:08 -0400

    draw a ball on the screen

--- app/src/main/java/com/example/individualassignment4q4p1/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment4q4p1/MainActivity.kt
@@ -8,6 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import com.example.individualassignment4q4p1.ui.theme.IndividualAssignment4Q4P1Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,5 +33,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen() {
-    // TODO
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val radius = 40f
+            val center = Offset(size.width / 2f, size.height / 2f)
+
+            drawCircle(
+                color = Color.Green,
+                radius = radius,
+                center = center
+            )
+        }
+    }
 }
